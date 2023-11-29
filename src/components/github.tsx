a3cb6cf1eaebc16b8e0b2d5a6ede229e1dfd1840
Yadong Xie <vthinkxie@gmail.com>
2023-11-29 16:11:17 +0800

    fix: update github link size

--- src/components/github.tsx
+++ src/components/github.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export function GithubForkRibbon() {
   return (
     <div className="overflow-hidden w-[150px] h-[150px] absolute top-0 z-50 right-0">
-      <div className="bg-zinc-100 text-zinc-950 font-bold shadow-2xl absolute p-2 z-50 top-[28px] right-[-55px] rotate-45">
+      <div className="bg-amber-600 text-zinc-100 shadow-2xl absolute p-1 z-50 top-[35px] right-[-45px] rotate-45">
         <a
-          className="w-[200px] inline-block p-2 text-center"
+          className="w-[200px] inline-block p-1 text-center"
           href="https://github.com/leptonai/tryemoji"
           target="_blank"
         >
